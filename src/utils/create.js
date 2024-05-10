7cf9b4dcd73f932b66079470dbf9bb838e3ca725
waitInHope <1064600154@qq.com>
2024-05-10 19:31:56 +0800

    feat: 引入加载图标表示拉取代码的过程

--- src/utils/create.js
+++ src/utils/create.js
@@ -1,6 +1,7 @@
 const promptList = require('./promptList');
 const { cloneGitRepo } = require('./git');
 const { templateProject } = require('./constants');
+const { createFuncLoadingByOra } = require('./loading'); 
 
 const create = async (name) => {
   console.log('子应用名称是:', name);
@@ -10,7 +11,7 @@ const create = async (name) => {
   console.log('你选择的框架是:', framework);
   if (templateProject[framework]) {
     console.log('开始拉取代码');
-    const res = cloneGitRepo(templateProject[framework], name, 'master');
+    const res = createFuncLoadingByOra(() => cloneGitRepo(templateProject[framework], name, 'master'), '拉取代码');
     if (res) {
       console.log('创建子应用完成', name);
     }
